5398519a94f80dc887f628787c4aab67ffa3cf10
Jorge Silva <jorge.silva.jr1@gmail.com>
2018-04-26 02:37:01 +0000

    posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#home'
   get '/about' => 'pages#about'
+  get '/posts' => 'pages#posts'
+
 end
